794afbcae6ce2d176a41596600882b018591d533
mesutyanmaz <71411632+mesutyanmaz@users.noreply.github.com>
2025-02-06 18:14:59 +0300

    fish kodlaması başlandı.

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -1,16 +1,53 @@
+using DG.Tweening;
 using UnityEngine;
+using System;
 
 public class Fish : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Fish.FishType type;
+
+    private CircleCollider2D coll;
+
+    private SpriteRenderer rend;
+
+    private float screenLeft;
+
+    private Tweener tweener;
+
+    public Fish.FishType Type
     {
-        
+        get { return type; }
+        set
+        {
+            type = value;
+            coll.radius = type.colliderRadius;
+            rend.sprite = type.sprite;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        coll = GetComponent<CircleCollider2D>();
+        rend = GetComponentInChildren<SpriteRenderer>();
+        screenLeft = Camera.main.ScreenToWorldPoint(Vector3.zero).x;
+
+    }
+
+
+    public void Hooked()
+    {
+        coll.enabled = false;
+        tweener.Kill(false);
+    }
+
+    [Serializable]
+    public class FishType //fishtype object's attribute
+    {
+        public int price;
+        public float fishCount;
+        public float minLength;
+        public float maxLength;
+        public float colliderRadius;
+        public Sprite sprite;
     }
 }
